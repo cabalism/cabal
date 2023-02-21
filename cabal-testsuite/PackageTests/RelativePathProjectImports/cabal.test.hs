f5e052af019cb13d0a3ee1eb53085347df89c654
Phil de Joux <phil.dejoux@blockscope.com>
2023-02-21 12:43:01 -0500

    Rename dep1. Build all, clean and then build each dep.

--- cabal-testsuite/PackageTests/RelativePathProjectImports/cabal.test.hs
+++ cabal-testsuite/PackageTests/RelativePathProjectImports/cabal.test.hs
@@ -2,4 +2,7 @@ import Test.Cabal.Prelude
 
 main = cabalTest $ do
   -- Build from toplevel.
-  cabal "build" []
+  cabal "build" ["all"]
+  cabal "clean" []
+  cabal "build" ["dep1"]
+  cabal "build" ["dep2"]
