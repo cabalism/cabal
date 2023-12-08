0284ba72ba95904f8a9bd0d1727eba253e2afb6a
Phil de Joux <philderbeast@gmail.com>
2023-12-08 12:07:16 -0500

    Add Show [LabeledPackageConstraint] instance

--- cabal-install-solver/src/Distribution/Solver/Types/LabeledPackageConstraint.hs
+++ cabal-install-solver/src/Distribution/Solver/Types/LabeledPackageConstraint.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Distribution.Solver.Types.LabeledPackageConstraint
     ( LabeledPackageConstraint(..)
     , unlabelPackageConstraint
     ) where
 
+import Distribution.Compat.Prelude
+import Prelude ()
 import Distribution.Solver.Types.ConstraintSource
 import Distribution.Solver.Types.PackageConstraint
 import Distribution.Types.PackageName
@@ -11,12 +15,20 @@ import Distribution.Types.PackageName
 data LabeledPackageConstraint
    = LabeledPackageConstraint PackageConstraint ConstraintSource
 
+instance {-# INCOHERENT #-} Show [LabeledPackageConstraint] where
+    show [] = ""
+    show xs = intercalate "\n" $
+        filter (/= "") (showLabeledPackageConstraint <$> xs)
+
 instance Show LabeledPackageConstraint where
-    show (LabeledPackageConstraint pc@(PackageConstraint scope _) src@ConstraintSourceProjectConfig{}) =
-        if unPackageName (scopeToPackageName scope) == "hashable"
-            then "SOURCE: " ++ showConstraintSource src ++ ", CONSTRAINT: " ++ show pc ++ "\n"
-            else "\n"
-    show LabeledPackageConstraint{} = "\n"
+    show = showLabeledPackageConstraint
+
+showLabeledPackageConstraint :: LabeledPackageConstraint -> String
+showLabeledPackageConstraint (LabeledPackageConstraint pc@(PackageConstraint scope _) src@ConstraintSourceProjectConfig{}) =
+    if unPackageName (scopeToPackageName scope) == "hashable"
+        then "SOURCE: " ++ showConstraintSource src ++ ", CONSTRAINT: " ++ show pc
+        else ""
+showLabeledPackageConstraint _ = ""
 
 unlabelPackageConstraint :: LabeledPackageConstraint -> PackageConstraint
 unlabelPackageConstraint (LabeledPackageConstraint pc _) = pc
