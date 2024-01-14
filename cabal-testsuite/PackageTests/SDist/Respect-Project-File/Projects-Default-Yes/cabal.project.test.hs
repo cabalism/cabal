22ba7b5d48acd2d4703f04aff2f7d77fb2a00ff5
Phil de Joux <philderbeast@gmail.com>
2024-01-14 15:45:20 -0500

    Leave a note explaining cabal.project.test.hs

--- cabal-testsuite/PackageTests/SDist/Respect-Project-File/Projects-Default-Yes/cabal.project.test.hs
+++ cabal-testsuite/PackageTests/SDist/Respect-Project-File/Projects-Default-Yes/cabal.project.test.hs
@@ -1,4 +1,6 @@
 import Test.Cabal.Prelude
 
+-- The given cabal.project has has "packages: p/ q/" and "cabal sdist" writes
+-- sdist/p-0.1.tar.gz and sdist/q-0.1.tar.gz.  This is correct.
 main = cabalTest . withProjectFile "cabal.project" $ do
     cabal "sdist" ["all"]
