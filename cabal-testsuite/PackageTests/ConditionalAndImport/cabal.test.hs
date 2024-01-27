d4ddd9317fd91a4b7e26df3abdfec6c000b953fe
Phil de Joux <philderbeast@gmail.com>
2024-01-27 13:58:26 -0500

    Add logging and assertions on output to cyclical test

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -1,8 +1,16 @@
 import Test.Cabal.Prelude
-main = cabalTest $
-  withRepo "repo" $ do
-     cabal "v2-run" [ "some-exe" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-loopback.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-1-hop.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-2-hop.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-bad-conditional.project" ]
+
+main = cabalTest . withRepo "repo" . recordMode RecordMarked $ do
+  let log = recordHeader . pure
+
+  cabal "v2-run" [ "some-exe" ]
+
+  log "checking cyclical loopback"
+  cyclical <- fails $ cabal' "v2-build" [ "--project-file=cabal-cyclical.project" ]
+  assertOutputContains "cyclical import of cabal-cyclical.project" cyclical
+
+  log "checking bad conditional"
+  badIf <- fails $ cabal' "v2-build" [ "--project-file=cabal-bad-conditional.project" ]
+  assertOutputContains "Cannot set compiler in a conditional clause of a cabal project file" badIf
+
+  return ()
