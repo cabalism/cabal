c2e05f8d3eb95d7d204fedc564de2fbb6872c208
Phil de Joux <philderbeast@gmail.com>
2024-01-27 12:07:20 -0500

    Add cyclical import tests with 1 and 2 hops in cycle

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -2,5 +2,7 @@ import Test.Cabal.Prelude
 main = cabalTest $
   withRepo "repo" $ do
      cabal "v2-run" [ "some-exe" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical.project" ]
+     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-loopback.project" ]
+     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-1-hop.project" ]
+     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-2-hop.project" ]
      fails $ cabal "v2-build" [ "--project-file=cabal-bad-conditional.project" ]
