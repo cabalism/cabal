9b82855de10e3276bdbe35286fe0ca71fb6d2934
Phil de Joux <philderbeast@gmail.com>
2024-01-27 13:49:52 -0500

    Add logging and assertOutputContains to cyclical tests

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -1,8 +1,24 @@
 import Test.Cabal.Prelude
-main = cabalTest $
-  withRepo "repo" $ do
-     cabal "v2-run" [ "some-exe" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-loopback.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-1-hop.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-cyclical-2-hop.project" ]
-     fails $ cabal "v2-build" [ "--project-file=cabal-bad-conditional.project" ]
+
+main = cabalTest . withRepo "repo" . recordMode RecordMarked $ do
+  let log = recordHeader . pure
+
+  cabal "v2-run" [ "some-exe" ]
+
+  log "checking cyclical loopback"
+  cyclical0 <- fails $ cabal' "v2-build" [ "--project-file=cabal-cyclical-loopback.project" ]
+  assertOutputContains "cyclical import of" cyclical0
+
+  log "checking cyclical with 1 hop"
+  cyclical1 <- fails $ cabal' "v2-build" [ "--project-file=cabal-cyclical-1-hop.project" ]
+  assertOutputContains "cyclical import of" cyclical1
+
+  log "checking cyclical with 2 hops"
+  cyclical2 <- fails $ cabal' "v2-build" [ "--project-file=cabal-cyclical-2-hop.project" ]
+  assertOutputContains "cyclical import of" cyclical2
+
+  log "checking bad conditional"
+  badIf <- fails $ cabal' "v2-build" [ "--project-file=cabal-bad-conditional.project" ]
+  assertOutputContains "Cannot set compiler in a conditional clause of a cabal project file" badIf
+
+  return ()
