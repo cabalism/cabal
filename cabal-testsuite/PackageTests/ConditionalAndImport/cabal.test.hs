672e76afdef28a073fca52d01d7bc059aaaed22d
Phil de Joux <philderbeast@gmail.com>
2024-01-29 14:29:20 -0500

    Add cyclical checks with same file names and hops
    - Add noncyclical tests that hop over folders
    - Add a project testing skipping in and out of a folder
    - Update expectations of cyclical tests

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -55,6 +55,71 @@ main = cabalTest . withRepo "repo" . recordMode RecordMarked $ do
   cyclical2c <- fails $ cabal' "v2-build" [ "--project-file=cyclical-2-out-out-self.project" ]
   assertOutputContains "cyclical import of cyclical-2-out-out-self-b.config" cyclical2c
 
+  -- +-- noncyclical-same-filename-a.project
+  --  +-- noncyclical-same-filename-a.config
+  --    +-- same-filename/noncyclical-same-filename-a.config (no further imports so not cyclical)
+  log "checking that cyclical check doesn't false-positive on same file names in different folders; hoping within a folder and then into a subfolder"
+  cyclical3b <- cabal' "v2-build" [ "--project-file=noncyclical-same-filename-a.project" ]
+  assertOutputDoesNotContain "cyclical import of" cyclical3b
+
+  -- +-- noncyclical-same-filename-b.project
+  --  +-- same-filename/noncyclical-same-filename-b.config
+  --    +-- noncyclical-same-filename-b.config (no further imports so not cyclical)
+  log "checking that cyclical check doesn't false-positive on same file names in different folders; hoping into a subfolder and then back out again"
+  cyclical3c <- fails $ cabal' "v2-build" [ "--project-file=noncyclical-same-filename-b.project" ]
+  assertOutputDoesNotContain "cyclical import of" cyclical3c
+
+  -- +-- cyclical-same-filename-out-out-self.project
+  --  +-- cyclical-same-filename-out-out-self.config
+  --    +-- same-filename/cyclical-same-filename-out-out-self.config
+  --    +-- same-filename/cyclical-same-filename-out-out-self.config (already processed)
+  --    +-- etc
+  log "checking that cyclical check catches a same file name that imports itself"
+  cyclical4a <- fails $ cabal' "v2-build" [ "--project-file=cyclical-same-filename-out-out-self.project" ]
+  assertOutputContains "cyclical import of cyclical-same-filename-out-out-self.config" cyclical4a
+
+  -- +-- cyclical-same-filename-out-out-backback.project
+  --  +-- cyclical-same-filename-out-out-backback.config
+  --    +-- same-filename/cyclical-same-filename-out-out-backback.config
+  -- +-- cyclical-same-filename-out-out-backback.project (already processed)
+  -- +-- etc
+  log "checking that cyclical check catches importing its importer (with the same file name)"
+  cyclical4b <- fails $ cabal' "v2-build" [ "--project-file=cyclical-same-filename-out-out-backback.project" ]
+  assertOutputContains "cyclical import of cyclical-same-filename-out-out-backback.project" cyclical4b
+
+  -- +-- cyclical-same-filename-out-out-back.project
+  --  +-- cyclical-same-filename-out-out-back.config
+  --    +-- same-filename/cyclical-same-filename-out-out-back.config
+  --  +-- cyclical-same-filename-out-out-back.config (already processed)
+  --  +-- etc
+  log "checking that cyclical check catches importing its importer's importer (hopping over same file names)"
+  cyclical4c <- fails $ cabal' "v2-build" [ "--project-file=cyclical-same-filename-out-out-back.project" ]
+  assertOutputContains "cyclical import of cyclical-same-filename-out-out-back.config" cyclical4c
+
+  -- +-- hops-0.project
+  --  +-- hops/hops-1.config
+  --   +-- hops-2.config
+  --    +-- hops/hops-3.config
+  --     +-- hops-4.config
+  --      +-- hops/hops-5.config
+  --       +-- hops-6.config
+  --        +-- hops/hops-7.config
+  --         +-- hops-8.config
+  --          +-- hops/hops-9.config (no further imports so not cyclical)
+  log "checking that imports work skipping into a subfolder and then back out again and again"
+  hopping <- cabal' "v2-build" [ "--project-file=hops-0.project" ]
+  assertOutputContains "this build was affected by the following (project) config files:" hopping
+  assertOutputContains "hops-0.project" hopping
+  assertOutputContains "../hops-2.config" hopping
+  assertOutputContains "../hops-4.config" hopping
+  assertOutputContains "../hops-6.config" hopping
+  assertOutputContains "../hops-8.config" hopping
+  assertOutputContains "hops/hops-1.config" hopping
+  assertOutputContains "hops/hops-3.config" hopping
+  assertOutputContains "hops/hops-5.config" hopping
+  assertOutputContains "hops/hops-7.config" hopping
+  assertOutputContains "hops/hops-9.config" hopping
+
   log "checking bad conditional"
   badIf <- fails $ cabal' "v2-build" [ "--project-file=bad-conditional.project" ]
   assertOutputContains "Cannot set compiler in a conditional clause of a cabal project file" badIf
