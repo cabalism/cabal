225429abc2f03ab0297b00e0d3ec0f21bef4c277
Phil de Joux <philderbeast@gmail.com>
2024-01-31 14:26:09 -0500

    .

--- cabal-install/src/Distribution/Client/ProjectConfig/Legacy.hs
+++ cabal-install/src/Distribution/Client/ProjectConfig/Legacy.hs
@@ -324,8 +324,7 @@ parseProjectSkeleton dir rootOrImport cacheDir httpTransport verbosity seenImpor
         createDirectoryIfMissing True cacheDir
         _ <- downloadURI httpTransport verbosity uri fp
         BS.readFile fp
-      Nothing ->
-        BS.readFile $ if isAbsolute pci then pci else dir </> pci
+      Nothing -> BS.readFile $ if isAbsolute pci then pci else dir </> pci
 
     modifiesCompiler :: ProjectConfig -> Bool
     modifiesCompiler pc = isSet projectConfigHcFlavor || isSet projectConfigHcPath || isSet projectConfigHcPkg
