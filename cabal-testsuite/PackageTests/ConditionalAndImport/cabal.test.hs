869b6706f5a7271ef75b1db29b7159cd00d6ca09
Phil de Joux <philderbeast@gmail.com>
2024-02-13 14:10:39 -0500

    Add Y-forking import test
    - A test for detecting when the same config is imported via many different paths
    - Error on duplicate imports
    - Do the filtering in duplicateImportMsg
    - Use duplicateImportMsg for cycles too
    - Add haddocks to IORef parameter
    - Add changelog entry
    - Use ordNub instead of nub
    - Use NubList
    - Share implement of duplicate and cyclical messages
    - Update expectation for non-cyclical duplicate import

--- cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ConditionalAndImport/cabal.test.hs
@@ -225,6 +225,37 @@ main = cabalTest . withRepo "repo" . recordMode RecordMarked $ do
     \        imported by: oops-0.project")
     oopsing
 
+  -- The project is named yops as it is like hops but with y's for forks.
+  -- +-- yops-0.project
+  --  +-- yops/yops-1.config
+  --   +-- yops-2.config
+  --    +-- yops/yops-3.config
+  --     +-- yops-4.config
+  --      +-- yops/yops-5.config
+  --       +-- yops-6.config
+  --        +-- yops/yops-7.config
+  --         +-- yops-8.config
+  --          +-- yops/yops-9.config (no further imports)
+  --  +-- yops/yops-3.config
+  --   +-- yops-4.config
+  --    +-- yops/yops-5.config
+  --     +-- yops-6.config
+  --      +-- yops/yops-7.config
+  --       +-- yops-8.config
+  --        +-- yops/yops-9.config (no further imports)
+  --  +-- yops/yops-5.config
+  --   +-- yops-6.config
+  --    +-- yops/yops-7.config
+  --     +-- yops-8.config
+  --      +-- yops/yops-9.config (no further imports)
+  --  +-- yops/yops-7.config
+  --   +-- yops-8.config
+  --    +-- yops/yops-9.config (no further imports)
+  --  +-- yops/yops-9.config (no further imports)
+  log "checking that we detect when the same config is imported via many different paths"
+  yopping <- fails $ cabal' "v2-build" [ "--project-file=yops-0.project" ]
+  assertOutputContains "duplicate import of yops/yops-3.config" yopping
+
   log "checking bad conditional"
   badIf <- fails $ cabal' "v2-build" [ "--project-file=bad-conditional.project" ]
   assertOutputContains "Cannot set compiler in a conditional clause of a cabal project file" badIf
