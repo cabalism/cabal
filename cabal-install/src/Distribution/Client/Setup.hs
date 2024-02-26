64f023ddf5970a40aa71434fba74ea462ceb8a93
Phil de Joux <philderbeast@gmail.com>
2024-02-26 09:56:19 -0500

    Copy sdist as target

--- cabal-install/src/Distribution/Client/Setup.hs
+++ cabal-install/src/Distribution/Client/Setup.hs
@@ -275,6 +275,7 @@ globalCommand commands =
               , "unpack"
               , "init"
               , "configure"
+              , "target"
               , "build"
               , "clean"
               , "run"
@@ -327,7 +328,8 @@ globalCommand commands =
               , "v1-register"
               , "v1-reconfigure"
               , -- v2 commands, nix-style
-                "v2-build"
+                "v2-target"
+              , "v2-build"
               , "v2-configure"
               , "v2-repl"
               , "v2-freeze"
@@ -381,6 +383,7 @@ globalCommand commands =
                 , addCmd "clean"
                 , par
                 , startGroup "running and testing"
+                , addCmd "target"
                 , addCmd "list-bin"
                 , addCmd "repl"
                 , addCmd "run"
@@ -412,6 +415,7 @@ globalCommand commands =
                 , addCmd "v2-install"
                 , addCmd "v2-clean"
                 , addCmd "v2-sdist"
+                , addCmd "v2-target"
                 , par
                 , startGroup "legacy command aliases"
                 , addCmd "v1-build"
