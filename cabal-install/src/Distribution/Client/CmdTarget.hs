6b5cca2af5daf39cb5ad40766e76dc322cb13321
Phil de Joux <philderbeast@gmail.com>
2024-02-26 10:04:59 -0500

    Strip, go with undefined

--- cabal-install/src/Distribution/Client/CmdTarget.hs
+++ cabal-install/src/Distribution/Client/CmdTarget.hs
@@ -8,8 +8,6 @@
 module Distribution.Client.CmdTarget
   ( targetCommand
   , targetAction
-  , packageToTarget
-  , OutputFormat (..)
   ) where
 
 import Distribution.Client.Compat.Prelude
@@ -165,53 +163,24 @@ targetCommand =
 -------------------------------------------------------------------------------
 
 data TargetFlags = TargetFlags
-  { targetVerbosity :: Flag Verbosity
-  , targetDistDir :: Flag FilePath
-  , targetListSources :: Flag Bool
-  , targetNulSeparated :: Flag Bool
-  , targetOutputPath :: Flag FilePath
+  { targetExplicit :: Flag Bool
   }
 
 defaultTargetFlags :: TargetFlags
 defaultTargetFlags =
   TargetFlags
-    { targetVerbosity = toFlag normal
-    , targetDistDir = mempty
-    , targetListSources = toFlag False
-    , targetNulSeparated = toFlag False
-    , targetOutputPath = mempty
+    { targetExplicit = toFlag False
     }
 
 targetOptions :: ShowOrParseArgs -> [OptionField TargetFlags]
 targetOptions showOrParseArgs =
-  [ optionVerbosity
-      targetVerbosity
-      (\v flags -> flags{targetVerbosity = v})
-  , optionDistPref
-      targetDistDir
-      (\dd flags -> flags{targetDistDir = dd})
-      showOrParseArgs
-  , option
-      ['l']
-      ["list-only"]
-      "Just list the sources, do not make a tarball"
-      targetListSources
-      (\v flags -> flags{targetListSources = v})
-      trueArg
-  , option
+  [ option
       []
-      ["null-sep"]
-      "Separate the source files with NUL bytes rather than newlines."
-      targetNulSeparated
-      (\v flags -> flags{targetNulSeparated = v})
+      ["explicit-only"]
+      "No abbreviations, only explicit forms."
+      targetExplicit
+      (\e flags -> flags{targetExplicit = e})
       trueArg
-  , option
-      ['o']
-      ["output-directory", "outputdir"]
-      "Choose the output directory of this command. '-' sends all output to stdout"
-      targetOutputPath
-      (\o flags -> flags{targetOutputPath = o})
-      (reqArg "PATH" (succeedReadE Flag) flagToList)
   ]
 
 -------------------------------------------------------------------------------
@@ -220,189 +189,4 @@ targetOptions showOrParseArgs =
 
 targetAction :: (ProjectFlags, TargetFlags) -> [String] -> GlobalFlags -> IO ()
 targetAction (pf@ProjectFlags{..}, TargetFlags{..}) targetStrings globalFlags = do
-  (baseCtx, distDirLayout) <-
-    withProjectOrGlobalConfig
-      flagIgnoreProject
-      withProject
-      (withGlobalConfig verbosity globalConfigFlag withoutProject)
-
-  let localPkgs = localPackages baseCtx
-
-  targetSelectors <-
-    either (reportTargetSelectorProblems verbosity) return
-      =<< readTargetSelectors localPkgs Nothing targetStrings
-
-  -- elaborate path, create target directory
-  mOutputPath' <- case mOutputPath of
-    Just "-" -> return (Just "-")
-    Just path -> do
-      abspath <- makeAbsolute path
-      createDirectoryIfMissing True abspath
-      return (Just abspath)
-    Nothing -> do
-      createDirectoryIfMissing True (distSdistDirectory distDirLayout)
-      return Nothing
-
-  let format :: OutputFormat
-      format =
-        if
-            | listSources, nulSeparated -> SourceList '\0'
-            | listSources -> SourceList '\n'
-            | otherwise -> TarGzArchive
-
-      ext = case format of
-        SourceList _ -> "list"
-        TarGzArchive -> "tar.gz"
-
-      outputPath pkg = case mOutputPath' of
-        Just path
-          | path == "-" -> "-"
-          | otherwise -> path </> prettyShow (packageId pkg) <.> ext
-        Nothing
-          | listSources -> "-"
-          | otherwise -> distSdistFile distDirLayout (packageId pkg)
-
-  case reifyTargetSelectors localPkgs targetSelectors of
-    Left errs -> dieWithException verbosity $ SdistActionException . fmap renderTargetProblem $ errs
-    Right pkgs
-      | length pkgs > 1
-      , not listSources
-      , Just "-" <- mOutputPath' ->
-          dieWithException verbosity Can'tWriteMultipleTarballs
-      | otherwise ->
-          traverse_ (\pkg -> packageToTarget verbosity (distProjectRootDirectory distDirLayout) format (outputPath pkg) pkg) pkgs
-  where
-    verbosity = fromFlagOrDefault normal targetVerbosity
-    listSources = fromFlagOrDefault False targetListSources
-    nulSeparated = fromFlagOrDefault False targetNulSeparated
-    mOutputPath = flagToMaybe targetOutputPath
-
-    prjConfig :: ProjectConfig
-    prjConfig =
-      commandLineFlagsToProjectConfig
-        globalFlags
-        (defaultNixStyleFlags ())
-          { configFlags =
-              (configFlags $ defaultNixStyleFlags ())
-                { configVerbosity = targetVerbosity
-                , configDistPref = targetDistDir
-                }
-          , projectFlags = pf
-          }
-        mempty
-
-    globalConfigFlag = projectConfigConfigFile (projectConfigShared prjConfig)
-
-    withProject :: IO (ProjectBaseContext, DistDirLayout)
-    withProject = do
-      baseCtx <- establishProjectBaseContext verbosity prjConfig OtherCommand
-      return (baseCtx, distDirLayout baseCtx)
-
-    withoutProject :: ProjectConfig -> IO (ProjectBaseContext, DistDirLayout)
-    withoutProject config = do
-      cwd <- getCurrentDirectory
-      baseCtx <- establishProjectBaseContextWithRoot verbosity (config <> prjConfig) (ProjectRootImplicit cwd) OtherCommand
-      return (baseCtx, distDirLayout baseCtx)
-
-data OutputFormat
-  = SourceList Char
-  | TarGzArchive
-  deriving (Show, Eq)
-
-packageToTarget :: Verbosity -> FilePath -> OutputFormat -> FilePath -> UnresolvedSourcePackage -> IO ()
-packageToTarget verbosity projectRootDir format outputFile pkg = do
-  let death = dieWithException verbosity $ ImpossibleHappened (show pkg)
-  dir0 <- case srcpkgSource pkg of
-    LocalUnpackedPackage path -> pure (Right path)
-    RemoteSourceRepoPackage _ (Just tgz) -> pure (Left tgz)
-    RemoteSourceRepoPackage{} -> death
-    LocalTarballPackage tgz -> pure (Left tgz)
-    RemoteTarballPackage _ (Just tgz) -> pure (Left tgz)
-    RemoteTarballPackage{} -> death
-    RepoTarballPackage{} -> death
-
-  let
-    -- Write String to stdout or file, using the default TextEncoding.
-    write str
-      | outputFile == "-" = putStr (withOutputMarker verbosity str)
-      | otherwise = do
-          writeFile outputFile str
-          notice verbosity $ "Wrote source list to " ++ outputFile ++ "\n"
-    -- Write raw ByteString to stdout or file as it is, without encoding.
-    writeLBS lbs
-      | outputFile == "-" = BSL.putStr lbs
-      | otherwise = do
-          BSL.writeFile outputFile lbs
-          notice verbosity $ "Wrote tarball sdist to " ++ outputFile ++ "\n"
-
-  case dir0 of
-    Left tgz -> do
-      case format of
-        TarGzArchive -> do
-          writeLBS =<< BSL.readFile tgz
-        _ -> dieWithException verbosity $ CannotConvertTarballPackage (show format)
-    Right dir -> case format of
-      SourceList nulSep -> do
-        let gpd :: GenericPackageDescription
-            gpd = srcpkgDescription pkg
-
-        files' <- listPackageSourcesWithDie verbosity dieWithException dir (flattenPackageDescription gpd) knownSuffixHandlers
-        let files = nub $ sort $ map normalise files'
-        let prefix = makeRelative projectRootDir dir
-        write $ concat [prefix </> i ++ [nulSep] | i <- files]
-      TarGzArchive -> do
-        packageDirToSdist verbosity (srcpkgDescription pkg) dir >>= writeLBS
-
---
-
-reifyTargetSelectors :: [PackageSpecifier UnresolvedSourcePackage] -> [TargetSelector] -> Either [TargetProblem] [UnresolvedSourcePackage]
-reifyTargetSelectors pkgs sels =
-  case partitionEithers (foldMap go sels) of
-    ([], sels') -> Right sels'
-    (errs, _) -> Left errs
-  where
-    -- there can be pkgs which are in extra-packages:
-    -- these are not SpecificSourcePackage
-    --
-    -- Why these packages are in localPkgs, it's confusing.
-    -- Anyhow, better to be lenient here.
-    --
-    flatten (SpecificSourcePackage pkg@SourcePackage{}) = Just pkg
-    flatten _ = Nothing
-
-    pkgs' = mapMaybe flatten pkgs
-
-    getPkg pid = case find ((== pid) . packageId) pkgs' of
-      Just pkg -> Right pkg
-      Nothing -> error "The impossible happened: we have a reference to a local package that isn't in localPackages."
-
-    go :: TargetSelector -> [Either TargetProblem UnresolvedSourcePackage]
-    go (TargetPackage _ pids Nothing) = fmap getPkg pids
-    go (TargetAllPackages Nothing) = Right <$> pkgs'
-    go (TargetPackage _ _ (Just kind)) = [Left (AllComponentsOnly kind)]
-    go (TargetAllPackages (Just kind)) = [Left (AllComponentsOnly kind)]
-    go (TargetPackageNamed pname _) = [Left (NonlocalPackageNotAllowed pname)]
-    go (TargetComponentUnknown pname _ _) = [Left (NonlocalPackageNotAllowed pname)]
-    go (TargetComponent _ cname _) = [Left (ComponentsNotAllowed cname)]
-
-data TargetProblem
-  = AllComponentsOnly ComponentKind
-  | NonlocalPackageNotAllowed PackageName
-  | ComponentsNotAllowed ComponentName
-
-renderTargetProblem :: TargetProblem -> String
-renderTargetProblem (AllComponentsOnly kind) =
-  "It is not possible to package only the "
-    ++ renderComponentKind Plural kind
-    ++ " from a package "
-    ++ "for distribution. Only entire packages may be packaged for distribution."
-renderTargetProblem (ComponentsNotAllowed cname) =
-  "The component "
-    ++ showComponentName cname
-    ++ " cannot be packaged for distribution on its own. "
-    ++ "Only entire packages may be packaged for distribution."
-renderTargetProblem (NonlocalPackageNotAllowed pname) =
-  "The package "
-    ++ unPackageName pname
-    ++ " cannot be packaged for distribution, because it is not "
-    ++ "local to this project."
+  undefined
