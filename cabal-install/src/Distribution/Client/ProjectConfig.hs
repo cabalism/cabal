b24f65a3effdab3a538f5e56f8300c439bde7e48
Phil de Joux <philderbeast@gmail.com>
2024-12-15 06:59:16 -0500

    Move ProjectParseResult into its own module

--- cabal-install/src/Distribution/Client/ProjectConfig.hs
+++ cabal-install/src/Distribution/Client/ProjectConfig.hs
@@ -138,9 +138,11 @@ import Distribution.Client.Utils
   ( determineNumJobs
   )
 import qualified Distribution.Deprecated.ParseUtils as OldParser
-  ( ProjectParseResult (..)
-  , locatedErrorMsg
+  ( locatedErrorMsg
   , showPWarning
+  )
+import qualified Distribution.Deprecated.ProjectParseUtils as OldParser
+  ( ProjectParseResult (..)
   )
 import Distribution.Fields
   ( PError
