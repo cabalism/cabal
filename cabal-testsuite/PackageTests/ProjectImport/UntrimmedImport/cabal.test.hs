33dfd9aa5849d12617afdb14eeb6e9d753f366cc
Phil de Joux <philderbeast@gmail.com>
2025-05-10 08:34:35 -0400

    ...

--- cabal-testsuite/PackageTests/ProjectImport/UntrimmedImport/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/UntrimmedImport/cabal.test.hs
@@ -3,12 +3,18 @@ import Test.Cabal.Prelude
 main = cabalTest . recordMode RecordMarked $ do
   let log = recordHeader . pure
 
-  log "checking project import with trailing space"
-  trailing <- cabal' "v2-build" [ "--dry-run", "--project-file=trailing-space.project" ]
+  log "checking project import with trailing space with file scheme"
+  trailing <- cabal' "v2-build" [ "--dry-run", "--project-file=trailing-space-file-scheme.project" ]
   assertOutputContains "import has leading or trailing whitespace" trailing
-  assertOutputContains "'https://www.stackage.org/nightly-2024-12-05/cabal.config '" trailing
+  assertOutputContains "'file:/nightly-2024-12-05.config '" trailing
+  assertOutputContains "'file:///nightly-2024-12-05.config '" trailing
 
-  log "checking project import with tabs and spaces"
-  cabal "v2-build" [ "--dry-run", "--project-file=tabs-and-spaces.project" ]
+  -- log "checking project import with trailing space with https scheme"
+  -- trailing <- cabal' "v2-build" [ "--dry-run", "--project-file=trailing-space.project" ]
+  -- assertOutputContains "import has leading or trailing whitespace" trailing
+  -- assertOutputContains "'https://www.stackage.org/nightly-2024-12-05/cabal.config '" trailing
+
+  -- log "checking project import with tabs and spaces"
+  -- cabal "v2-build" [ "--dry-run", "--project-file=tabs-and-spaces.project" ]
 
   return ()
