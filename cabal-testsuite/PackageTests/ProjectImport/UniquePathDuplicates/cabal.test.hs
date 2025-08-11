814b84cc190a9adfa424108a4c74fe9bae2f8a0f
Phil de Joux <philderbeast@gmail.com>
2025-08-11 13:34:34 -0400

    Use </> for paths

--- cabal-testsuite/PackageTests/ProjectImport/UniquePathDuplicates/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/UniquePathDuplicates/cabal.test.hs
@@ -39,20 +39,20 @@ main = cabalTest . recordMode RecordMarked $ do
   assertOutputContains "Warning: 2 imports of yops-4.config" yopping
   assertOutputContains "Warning: 2 imports of yops-6.config" yopping
   assertOutputContains "Warning: 2 imports of yops-8.config" yopping
-  assertOutputContains "Warning: 2 imports of yops/yops-3.config" yopping
-  assertOutputContains "Warning: 2 imports of yops/yops-5.config" yopping
-  assertOutputContains "Warning: 2 imports of yops/yops-7.config" yopping
-  assertOutputContains "Warning: 2 imports of yops/yops-9.config" yopping
+  assertOutputContains ("Warning: 2 imports of " ++ ("yops" </> "yops-3.config")) yopping
+  assertOutputContains ("Warning: 2 imports of " ++ ("yops" </> "yops-5.config")) yopping
+  assertOutputContains ("Warning: 2 imports of " ++ ("yops" </> "yops-7.config")) yopping
+  assertOutputContains ("Warning: 2 imports of " ++ ("yops" </> "yops-9.config")) yopping
 
   log "checking that we detect when the same config is imported via many different paths"
   wooping <- cabal' "v2-build" [ "--project-file=woops-0.project", "--project-file-parser=legacy", "--dry-run" ]
   assertOutputContains "Warning: 2 imports of woops-4.config" wooping
   assertOutputContains "Warning: 2 imports of woops-6.config" wooping
   assertOutputContains "Warning: 2 imports of woops-8.config" wooping
-  assertOutputContains "Warning: 2 imports of woops/woops-3.config" wooping
-  assertOutputContains "Warning: 2 imports of woops/woops-5.config" wooping
-  assertOutputContains "Warning: 2 imports of woops/woops-7.config" wooping
-  assertOutputContains "Warning: 2 imports of woops/woops-9.config" wooping
-  assertOutputContains "Warning: 10 imports of www-stackage-org/lts-21.25.config" wooping
+  assertOutputContains ("Warning: 2 imports of " ++ ("woops" </> "woops-3.config")) wooping
+  assertOutputContains ("Warning: 2 imports of " ++ ("woops" </> "woops-5.config")) wooping
+  assertOutputContains ("Warning: 2 imports of " ++ ("woops" </> "woops-7.config")) wooping
+  assertOutputContains ("Warning: 2 imports of " ++ ("woops" </> "woops-9.config")) wooping
+  assertOutputContains ("Warning: 10 imports of " ++ ("www-stackage-org" </> "lts-21.25.config")) wooping
 
   return ()
