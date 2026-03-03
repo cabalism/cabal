17be5238d792a92f9c8ea5127023ce8a26912edd
Julian Ospald <hasufell@posteo.de>
2026-03-03 17:36:19 +0800

    Add test for #10255

---
+++ cabal-testsuite/PackageTests/ProjectImport/FileMonitoring/cabal.test.hs
@@ -0,0 +1,15 @@
+import Test.Cabal.Prelude
+import System.Exit (ExitCode(ExitFailure))
+import System.IO
+
+main = cabalTest (withProjectFile "cabal.project" $ do
+  result <- fails $ recordMode DoNotRecord $ cabal' "build" []
+  assertExitCode (ExitFailure 1) result
+  assertOutputContains "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." result
+
+  -- change the imported project file
+  test_dir <- fmap testTmpDir getTestEnv
+  liftIO $ writeFile (test_dir </> "nested" </> "cabal.project.common") "package *\n  Tests: False"
+  result' <- recordMode DoNotRecord $ cabal' "build" []
+  assertOutputDoesNotContain "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." result'
+  )
