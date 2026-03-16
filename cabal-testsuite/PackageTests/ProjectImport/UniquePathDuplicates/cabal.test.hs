2d568bd1521f3959c59e8d64b8107c8d477276c0
Phil de Joux <philderbeast@gmail.com>
2026-03-16 14:23:42 -0400

    Add script tests

--- cabal-testsuite/PackageTests/ProjectImport/UniquePathDuplicates/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/UniquePathDuplicates/cabal.test.hs
@@ -33,16 +33,26 @@ import Data.List (isInfixOf)
 --  +-- yops/yops-9.config (no further imports)
 main = do
   -- NOTE: Options are --project-file-parser=<legacy|default|parsec|fallback|compare>
-  cabalTest' "parser-legacy" . recordMode RecordMarked $ runTest "legacy"
-  cabalTest' "parser-default" . recordMode RecordMarked $ runTest "default"
-  cabalTest' "parser-parsec" . recordMode RecordMarked $ runTest "parsec"
-  cabalTest' "parser-fallback" . recordMode RecordMarked $ runTest "fallback"
+  cabalTest' "parser-legacy" . recordMode RecordMarked $ runTest (yProjectOpts "legacy") (wProjectOpts "legacy")
+  cabalTest' "parser-default" . recordMode RecordMarked $ runTest (yProjectOpts "default") (wProjectOpts "default") 
+  cabalTest' "parser-parsec" . recordMode RecordMarked $ runTest (yProjectOpts "parsec") (wProjectOpts "parsec")
+  cabalTest' "parser-fallback" . recordMode RecordMarked $ runTest (yProjectOpts "fallback") (wProjectOpts "fallback")
+
+  cabalTest' "script-parser-legacy" . recordMode RecordMarked $ runTest (yScriptOpts "legacy") (wScriptOpts "legacy")
+  cabalTest' "script-parser-default" . recordMode RecordMarked $ runTest (yScriptOpts "default") (wScriptOpts "default")
+  cabalTest' "script-parser-parsec" . recordMode RecordMarked $ runTest (yScriptOpts "parsec") (wScriptOpts "parsec")
+  cabalTest' "script-parser-fallback" . recordMode RecordMarked $ runTest (yScriptOpts "fallback") (wScriptOpts "fallback")
   where
     log = recordHeader . pure
 
-    runTest parser = do
+    yProjectOpts parser = [ "--project-file=yops-0.project", "--project-file-parser=" ++ parser, "--dry-run" ]
+    wProjectOpts parser = [ "--project-file=woops-0.project", "--project-file-parser=" ++ parser, "--dry-run" ]
+    yScriptOpts parser = [ "yops-0.script.hs", "--project-file-parser=" ++ parser, "--dry-run" ]
+    wScriptOpts parser = [ "woops-0.script.hs", "--project-file-parser=" ++ parser, "--dry-run" ]
+
+    runTest yOpts wOpts = do
       log "checking that we detect when the same config is imported via many different paths"
-      yopping <- cabal' "v2-build" [ "--project-file=yops-0.project", "--project-file-parser=" ++ parser, "--dry-run" ]
+      yopping <- cabal' "v2-build" yOpts
       assertOutputContains "Warning: 2 imports of yops-4.config" yopping
       assertOutputContains "Warning: 3 imports of yops-6.config" yopping
       assertOutputContains "Warning: 4 imports of yops-8.config" yopping
@@ -52,7 +62,7 @@ main = do
       assertOutputContains ("Warning: 5 imports of " ++ ("yops" </> "yops-9.config")) yopping
 
       log "checking that we detect when the same config is imported via many different paths"
-      wooping <- cabal' "v2-build" [ "--project-file=woops-0.project", "--project-file-parser=" ++ parser, "--dry-run" ]
+      wooping <- cabal' "v2-build" wOpts
       assertOutputContains "Warning: 2 imports of woops-4.config" wooping
       assertOutputContains "Warning: 3 imports of woops-6.config" wooping
       assertOutputContains "Warning: 4 imports of woops-8.config" wooping
