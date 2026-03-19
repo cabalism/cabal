6b02d55f0208ad4d492e0c1dcb0450d560bf7cc1
Phil de Joux <philderbeast@gmail.com>
2026-03-19 13:53:33 -0400

    Fix sorting of ProjectConfigPath

--- cabal-install-solver/src/Distribution/Solver/Types/ProjectConfigPath.hs
+++ cabal-install-solver/src/Distribution/Solver/Types/ProjectConfigPath.hs
@@ -66,11 +66,6 @@ instance Pretty ProjectImport where
 instance Show ProjectImport where show = prettyShow
 
 -- | Sorts the same as 'ProjectConfigPath' does.
---
--- >>> let abcd = ProjectImport "D.config" (ProjectConfigPath $ "C.config" :| ["B.config", "A.project"])
--- >>> let bcde = ProjectImport "E.config" (ProjectConfigPath $ "D.config" :| ["C.config", "B.project"])
--- >>> (compare abcd bcde, let xs = [abcd, bcde] in xs == sort (reverse xs))
--- (LT,True)
 instance Ord ProjectImport where
     compare = compare `on` (\ProjectImport{..} -> consProjectConfigPath importOf importBy)
 
@@ -106,6 +101,21 @@ instance Show ProjectConfigPath where show = prettyShow
 -- >>> let abBwd = ProjectConfigPath $ "a\\b.config" :| []
 -- >>> compare abFwd abBwd
 -- EQ
+--
+-- >>> let abc = ProjectConfigPath $ "a/b/c.config" :| []
+-- >>> let yz = ProjectConfigPath $ "y/z.config" :| []
+-- >>> (compare abc yz, let xs = [abc, yz] in xs == sort xs)
+-- (GT,False)
+--
+-- >>> let abc = ProjectConfigPath $ "C.config" :| ["B.config", "A.project"]
+-- >>> let bcd = ProjectConfigPath $ "D.config" :| ["C.config", "B.project"]
+-- >>> (compare abc bcd, let xs = [abc, bcd] in xs == sort xs)
+-- (LT,True)
+--
+-- >>> let abc = ProjectConfigPath $ "C.config" :| ["B.config", "A.project"]
+-- >>> let yz = ProjectConfigPath $ "Z.config" :| ["Y.project"]
+-- >>> (compare abc yz, let xs = [abc, yz] in xs == sort xs)
+-- (GT,False)
 instance Ord ProjectConfigPath where
     compare pa@(ProjectConfigPath (NE.toList -> as)) pb@(ProjectConfigPath (NE.toList -> bs)) =
         case (as, bs) of
@@ -114,19 +124,33 @@ instance Ord ProjectConfigPath where
             -- this though, do a comparison anyway when both sides have length
             -- 1.  The root path, the project itself, should always be the first
             -- path in a sorted listing.
-            ([a], [b]) -> compare (splitPath a) (splitPath b)
+            ([a], [b]) ->
+                let aPaths = splitPath a
+                    bPaths = splitPath b
+                in
+                    compare (length aPaths) (length bPaths)
+                    P.<> compare aPaths bPaths
+
             ([_], _) -> LT
             (_, [_]) -> GT
+            ([], []) -> EQ
+            ([], _) -> LT
+            (_, []) -> GT
 
             (a:_, b:_) -> case (parseAbsoluteURI a, parseAbsoluteURI b) of
                 (Just ua, Just ub) -> compare ua ub P.<> compare aImporters bImporters
                 (Just _, Nothing) -> GT
                 (Nothing, Just _) -> LT
-                (Nothing, Nothing) -> compare (splitPath a) (splitPath b) P.<> compare aImporters bImporters
-            _ ->
-                compare (length as) (length bs)
-                P.<> compare (length aPaths) (length bPaths)
-                P.<> compare aPaths bPaths
+                (Nothing, Nothing) ->
+                    let aPaths = splitPath a
+                        bPaths = splitPath b
+                    in
+                        compare (length as) (length bs)
+                        P.<> compare (length asPaths) (length bsPaths)
+                        P.<> compare asPaths bsPaths
+                        P.<> compare (length aPaths) (length bPaths)
+                        P.<> compare aPaths bPaths
+                        P.<> compare aImporters bImporters
         where
             splitPath = FP.splitPath . normSep where
                 normSep p =
@@ -138,8 +162,8 @@ instance Ord ProjectConfigPath where
                             Posix.joinPath $ Posix.splitDirectories
                             [if Windows.isPathSeparator c then Posix.pathSeparator else c| c <- p]
 
-            aPaths = splitPath <$> as
-            bPaths = splitPath <$> bs
+            asPaths = splitPath <$> as
+            bsPaths = splitPath <$> bs
             aImporters = snd $ unconsProjectConfigPath pa
             bImporters = snd $ unconsProjectConfigPath pb
 
