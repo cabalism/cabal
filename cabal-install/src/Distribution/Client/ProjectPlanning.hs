8fb6d5ce98db568e40155faca29802efe077c24b
Phil de Joux <philderbeast@gmail.com>
2026-05-26 13:34:43 -0400

    Reduce diff

--- cabal-install/src/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/src/Distribution/Client/ProjectPlanning.hs
@@ -2487,9 +2487,6 @@ elaborateInstallPlan
 
             elabBenchmarkOptions = perPkgOptionList pkgid packageConfigBenchmarkOptions
 
-      perPkgOption :: (Package pkg, Monoid m) => pkg -> (PackageConfig -> m) -> m
-      perPkgOption = lookupPerPkgOption isLocalToProject allPackagesConfig localPackagesConfig perPackageConfig
-
       perPkgOptionFlag :: a -> PackageId -> (PackageConfig -> Flag a) -> a
       perPkgOptionFlag def = fmap (fromFlagOrDefault def) . perPkgOption
 
@@ -2502,6 +2499,9 @@ elaborateInstallPlan
       perPkgOptionLibExeFlag :: a -> PackageId -> (PackageConfig -> Flag a) -> (PackageConfig -> Flag a) -> (a, a)
       perPkgOptionLibExeFlag (fromFlagOrDefault -> f) pkgid (perPkgOption pkgid -> both) (perPkgOption pkgid -> lib) =
         (f both, f (both <> lib))
+
+      perPkgOption :: (Package pkg, Monoid m) => pkg -> (PackageConfig -> m) -> m
+      perPkgOption = lookupPerPkgOption isLocalToProject allPackagesConfig localPackagesConfig perPackageConfig
 
       inplacePackageDbs =
         corePackageDbs
