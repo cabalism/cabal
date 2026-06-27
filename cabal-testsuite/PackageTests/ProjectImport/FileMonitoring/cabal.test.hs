059b444247971db6c4d58a3590c530c0e2667782
Phil de Joux <philderbeast@gmail.com>
2026-06-27 08:37:06 -0400

    Rework test

--- cabal-testsuite/PackageTests/ProjectImport/FileMonitoring/cabal.test.hs
+++ cabal-testsuite/PackageTests/ProjectImport/FileMonitoring/cabal.test.hs
@@ -2,15 +2,28 @@ import Test.Cabal.Prelude
 import System.Exit (ExitCode(ExitFailure))
 import System.IO
 
-main = cabalTest (withProjectFile "cabal.project" $ do
-  result <- fails $ recordMode DoNotRecord $ cabal' "build" []
-  assertExitCode (ExitFailure 1) result
-  assertOutputContains "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." result
+-- If tests are enabled then we get this output:
+-- [1 of 1] Compiling Main
+-- test/Main.hs:4:8: error: [GHC-88464]
+--     Variable not in scope: puStrLn :: [Char] -> IO ()
+--     Suggested fix: Perhaps use ‘putStrLn’ (imported from Prelude)
+--   |
+-- 4 | main = puStrLn "Test suite not yet implemented."
+--   |
+main = cabalTest . recordMode RecordMarked $ do
+  projEnabledTests <- fails $ cabal' "build" []
+  assertExitCode (ExitFailure 1) projEnabledTests
+  assertOutputContains "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." projEnabledTests
 
-  -- change the imported project file
+  cmdDisabledTests <- cabal' "build" ["--disable-tests"]
+  assertOutputDoesNotContain "Test suite not yet implement" cmdDisabledTests
+
+  -- Change the imported project file with "Tests: False"
   test_dir <- fmap testTmpDir getTestEnv
   liftIO $ writeFile (test_dir </> "test" </> "tests-toggle.config") "package *\n  Tests: False"
-  result' <- recordMode DoNotRecord $ cabal' "build" []
-  assertOutputDoesNotContain "Test suite not yet implement" result'
-  assertOutputDoesNotContain "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." result'
-  )
+  projDisabledTests <- cabal' "build" []
+  assertOutputDoesNotContain "Test suite not yet implement" projDisabledTests
+  assertOutputDoesNotContain "Failed to build cabal-project-repro-0.1.0.0-inplace-cabal-project-repro-test." projDisabledTests
+
+  cmdEnabledTests <- fails $ cabal' "build" ["--enable-tests"]
+  assertOutputContains "Test suite not yet implement" cmdEnabledTests
