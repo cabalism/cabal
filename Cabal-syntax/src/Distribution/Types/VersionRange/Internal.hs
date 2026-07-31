8757d0787df89186db48fb8b463030d7c2ae6ef9
Phil de Joux <philderbeast@gmail.com>
2026-07-31 11:15:31 -0400

    Applicative intersect && union version ranges

--- Cabal-syntax/src/Distribution/Types/VersionRange/Internal.hs
+++ Cabal-syntax/src/Distribution/Types/VersionRange/Internal.hs
@@ -356,25 +356,12 @@ instance Parsec VersionRange where
 versionRangeParser :: forall m. CabalParsing m => m Int -> CabalSpecVersion -> m VersionRange
 versionRangeParser digitParser csv = expr
   where
-    expr = do
-      P.spaces
-      t <- term
-      P.spaces
-      do
-        _ <- P.string "||"
-        checkOp
-        P.spaces
-        unionVersionRanges t <$> expr
-        <|> return t
-    term = do
-      f <- factor
-      P.spaces
-      do
-        _ <- P.string "&&"
-        checkOp
-        P.spaces
-        intersectVersionRanges f <$> term
-        <|> return f
+    expr =
+      unionVersionRanges <$ P.spaces <*> term <* P.spaces <* P.string "||" <* checkOp <* P.spaces <*> expr
+        <|> term
+    term =
+      intersectVersionRanges <$> factor <* P.spaces <* P.string "&&" <* checkOp <* P.spaces <*> term
+        <|> factor
     factor = parens expr <|> prim
 
     prim = do
