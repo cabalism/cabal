f28cede5da61d2199f00ece3419ee71bad4610d3
Phil de Joux <philderbeast@gmail.com>
2026-08-03 15:34:22 -0400

    Add check of options for build command

--- cabal-install/tests/UnitTests.hs
+++ cabal-install/tests/UnitTests.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Test.Tasty
 
 import qualified UnitTests.Distribution.Client.BuildReport
+import qualified UnitTests.Distribution.Client.CmdBuildOptionsCommandUI
 import qualified UnitTests.Distribution.Client.Configure
 import qualified UnitTests.Distribution.Client.FetchUtils
 import qualified UnitTests.Distribution.Client.GZipUtils
@@ -35,6 +36,9 @@ main = do
       [ testGroup
           "UnitTests.Distribution.Client.BuildReport"
           UnitTests.Distribution.Client.BuildReport.tests
+      , testGroup
+          "UnitTests.Distribution.Client.CmdBuildOptionsCommandUI"
+          UnitTests.Distribution.Client.CmdBuildOptionsCommandUI.tests
       , testGroup
           "UnitTests.Distribution.Client.Configure"
           UnitTests.Distribution.Client.Configure.tests
---
+++ cabal-install/tests/UnitTests/Distribution/Client/CmdBuildOptionsCommandUI.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE LambdaCase #-}
+
+module UnitTests.Distribution.Client.CmdBuildOptionsCommandUI (tests) where
+
+import Distribution.Client.CmdBuild (buildCommand)
+import Distribution.Simple.Command
+  ( CommandParse (..)
+  , CommandUI (..)
+  , OptDescr (..)
+  , OptionField (..)
+  , ShowOrParseArgs (ParseArgs)
+  , commandParseArgs
+  )
+
+import Data.Char (toLower)
+import Data.List (isInfixOf)
+import Test.Tasty
+import Test.Tasty.HUnit
+
+tests :: [TestTree]
+tests =
+  [ testCase "build CommandUI parser recognizes options advertised by CommandUI(ParseArgs)" $ do
+      let probes = commandOptionProbes buildCommand
+          failures =
+            [ (probeDisplay probe, unlines errs)
+            | probe <- probes
+            , CommandErrors errs <- [commandParseArgs buildCommand False (probeArgv probe)]
+            , any isUnrecognizedError errs
+            ]
+
+      assertBool
+        ( "Unrecognized options found:\n"
+            <> unlines
+              [ "  " <> opt <> "\n" <> indent err
+              | (opt, err) <- failures
+              ]
+        )
+        (null failures)
+  ]
+
+type OptionProbe = (String, [String])
+
+probeDisplay :: OptionProbe -> String
+probeDisplay (display, _) = display
+
+probeArgv :: OptionProbe -> [String]
+probeArgv (_, argv) = argv
+
+commandOptionProbes :: CommandUI flags -> [OptionProbe]
+commandOptionProbes cmd =
+  concatMap optionFieldProbes (commandOptions cmd ParseArgs)
+
+optionFieldProbes :: OptionField flags -> [OptionProbe]
+optionFieldProbes (OptionField _ descrs) = concatMap optDescrProbes descrs
+
+optDescrProbes :: OptDescr flags -> [OptionProbe]
+optDescrProbes = \case
+  ReqArg _ (shortFlags, longFlags) _ _ _ ->
+    requiredArgProbes shortFlags longFlags
+  OptArg _ (shortFlags, longFlags) _ _ _ _ ->
+    noArgProbes shortFlags longFlags
+  ChoiceOpt choices ->
+    concatMap (\(_, (shortFlags, longFlags), _, _) -> noArgProbes shortFlags longFlags) choices
+  BoolOpt _ (shortTrue, longTrue) (shortFalse, longFalse) _ _ ->
+    noArgProbes shortTrue longTrue <> noArgProbes shortFalse longFalse
+
+requiredArgProbes :: [Char] -> [String] -> [OptionProbe]
+requiredArgProbes shortFlags longFlags =
+  [ ("--" <> longFlag, ["--" <> longFlag])
+  | longFlag <- longFlags
+  ]
+    <> [ ("-" <> [shortFlag], ["-" <> [shortFlag]])
+       | shortFlag <- shortFlags
+       ]
+
+noArgProbes :: [Char] -> [String] -> [OptionProbe]
+noArgProbes shortFlags longFlags =
+  [ ("--" <> longFlag, ["--" <> longFlag])
+  | longFlag <- longFlags
+  ]
+    <> [ ("-" <> [shortFlag], ["-" <> [shortFlag]])
+       | shortFlag <- shortFlags
+       ]
+
+isUnrecognizedError :: String -> Bool
+isUnrecognizedError msg =
+  let lower = map toLower msg
+   in "invalid option" `isInfixOf` lower
+        || "unrecognized" `isInfixOf` lower
+        || "unrecognised" `isInfixOf` lower
+        || "unknown option" `isInfixOf` lower
+
+indent :: String -> String
+indent = unlines . map ("    " <>) . lines
