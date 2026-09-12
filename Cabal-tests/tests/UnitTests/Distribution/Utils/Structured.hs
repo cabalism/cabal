ee4f493d50645daa43945037afb50468b9c3e922
Phil de Joux <philderbeast@gmail.com>
2026-09-11 21:01:16 -0400

    Update structured hash

--- Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
+++ Cabal-tests/tests/UnitTests/Distribution/Utils/Structured.hs
@@ -33,8 +33,8 @@ md5Check proxy md5Int = structureHash proxy @?= md5FromInteger md5Int
 
 md5CheckGenericPackageDescription :: Proxy GenericPackageDescription -> Assertion
 md5CheckGenericPackageDescription proxy = md5Check proxy
-    0x2ab5eeae7337cba494221e344aaaaafc
+    0x9e8139bb357e2c658060729846573dbc
 
 md5CheckLocalBuildInfo :: Proxy LocalBuildInfo -> Assertion
 md5CheckLocalBuildInfo proxy = md5Check proxy
-    0x3398bd7f316ecb8f535cbe78498a89a4
+    0x21e36bfd403d434deb706dd0058e7d41
